4617038781f6b645b3c245463b399b912319dfdb
Filippo Valli <filippovalli2003@gmail.com>
2022-05-28 18:34:45 +0200

    Fix fetchChats()

--- lib/providers/fetch_chats.dart
+++ lib/providers/fetch_chats.dart
@@ -6,53 +6,56 @@ import 'package:timezone/data/latest_all.dart';
 import 'package:timezone/timezone.dart';
 
 Future<List> fetchChats(bool isRefreshing) async {
+  // Check if user has chats stored locally
   if (user['chats'] != null && isRefreshing == false) {
-    // user has chats stored locally
+    // User has chats stored locally
+    // Return locally stored chats
 
     return user['chats'];
-  } else {
-    // user has to download chats
+  }
 
-    // instatiate reference of Cloud Firestore
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
+  // Instanciate reference of Cloud Firestore
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-    List _chats = [];
-    List _expiredChats = [];
+  List _activeChats = [];
+  List _expiredChats = [];
 
-    // fetch chats
-    await firestore
-        .collection('uids')
-        .doc(user['data']['uid'])
-        .collection('chats')
-        .get()
-        .then(
-      (QuerySnapshot querySnapshot) async {
-        Map _tempChats = {};
+  // Fetch chats
+  await firestore
+      .collection('uids')
+      .doc(user['data']['uid'])
+      .collection('chats')
+      .get()
+      .then(
+    (QuerySnapshot querySnapshot) async {
+      // Temporally store every chat
+      Map _tempChats = {};
+      for (DocumentSnapshot doc in querySnapshot.docs) {
+        _tempChats[doc.id] = doc.data();
+      }
 
-        for (DocumentSnapshot doc in querySnapshot.docs) {
-          _tempChats[doc.id] = doc.data();
-        }
+      // Check if there are chats
+      if (_tempChats != {}) {
+        for (Map _chat in _tempChats.values) {
+          // Fetch how many hours passed since last message
+          int hoursSinceLastMessage = DateTime.now()
+              .difference(DateTime.parse(await _chat['sent_at']))
+              .inHours;
 
-        if (_tempChats != {}) {
-          for (Map _chat in _tempChats.values) {
-            // check if chat is expired
-            int hoursSinceLast = DateTime.now()
-                .difference(DateTime.parse(await _chat['sent_at']))
-                .inHours;
-
-            bool isChatExpired = hoursSinceLast > 24;
-
-            if (isChatExpired) {
-              // chat is expired
+          // Check if chat is expired
+          if (hoursSinceLastMessage > 24) {
+            // Chat is expired
+            // Check if chat expiration was already handled
+            if (_chat['is_chat_expired'] == null) {
+              // Fetch chat key
               String chatKey = '';
-
               if (_chat['recipient_uid'] == user['data']['uid']) {
                 chatKey = _chat['sender_uid'];
               } else {
                 chatKey = _chat['recipient_uid'];
               }
 
-              // expire chat for user
+              // Expire chat for the current user
               await firestore
                   .collection('uids')
                   .doc(user['data']['uid'])
@@ -65,7 +68,7 @@ Future<List> fetchChats(bool isRefreshing) async {
                 },
               );
 
-              // expire chat for other user
+              // Expire chat for the other user
               await firestore
                   .collection('uids')
                   .doc(chatKey)
@@ -77,109 +80,140 @@ Future<List> fetchChats(bool isRefreshing) async {
                   'is_chat_expired': true,
                 },
               );
-              _expiredChats.add(_chat);
-            } else {
-              _chats.add(_chat);
             }
-          }
-        }
-      },
-    );
 
-    // sort chats List
-    _chats.sort(
-      (a, b) {
-        if (a['recipient_uid'] == user['data']['uid'] &&
-            b['recipient_uid'] == user['data']['uid']) {
-          if (DateTime.parse(a['sent_at'])
-              .isBefore(DateTime.parse(b['sent_at']))) {
-            return -1;
+            // Add chat to expired chats
+            _expiredChats.add(_chat);
           } else {
-            return 1;
-          }
-        } else if (a['sender_uid'] == user['data']['uid'] &&
-            b['sender_uid'] == user['data']['uid']) {
-          if (DateTime.parse(a['sent_at'])
-              .isBefore(DateTime.parse(b['sent_at']))) {
-            return -1;
-          } else {
-            return 1;
-          }
-        } else {
-          if (b['recipient_uid'] == user['data']['uid']) {
-            return 1;
-          } else {
-            return -1;
-          }
-        }
-      },
-    );
-
-    _chats = [..._expiredChats, ..._chats];
-
-    // update total_friends
-    if (user['data']['friends'] != null) {
-      await firestore.collection('uids').doc(user['data']['uid']).update(
-        {
-          'total_friends': user['data']['friends'].length,
-        },
-      );
-    } else {
-      await firestore.collection('uids').doc(user['data']['uid']).update(
-        {
-          'total_friends': 0,
-        },
-      );
-    }
-
-    user['chats'] = _chats;
-
-    // schedule expiring chat notifications
-    int index = 0;
-
-    for (Map _chat in _chats) {
-      // update index of _chats
-      index = index + 1;
-
-      // check if user is recipient of chat
-      if (_chat['recipient_uid'] == user['data']['uid']) {
-        final SharedPreferences prefs = await SharedPreferences.getInstance();
-        int? notificationId = prefs.getInt(_chat['sender_uid']);
-
-        // check if a notification for this chat already exists
-        if (notificationId == null) {
-          // fetch date for notification
-          initializeTimeZones();
-
-          TZDateTime notificationDate =
-              TZDateTime.parse(local, _chat['sent_at']).add(
-            const Duration(
-              minutes: 1380,
-            ),
-          );
-
-          // check if now is before scheduled notification
-          if (DateTime.now().toLocal().isBefore(notificationDate)) {
-            // generate uid for notification
-            notificationId =
-                DateTime.now().millisecondsSinceEpoch ~/ 1000 + index;
-
-            // store notification's uid locally
-            final SharedPreferences prefs =
-                await SharedPreferences.getInstance();
-            prefs.setInt(_chat['sender_uid'], notificationId);
-
-            // schedule local notification
-            LocalNotificationService.scheduleNotification(
-              _chat['sender_username'],
-              notificationId,
-              notificationDate,
-            );
+            // Chat is not expired
+            // Add chat to active chats
+            _activeChats.add(_chat);
           }
         }
       }
-    }
+    },
+  );
 
-    return _chats;
+  // Sort active chats
+  _activeChats.sort(
+    (a, b) {
+      // Check chats' data
+      if (a['recipient_uid'] == user['data']['uid'] &&
+          b['recipient_uid'] == user['data']['uid']) {
+        // User is recipient of both chats
+        // Order chats chronologically
+        if (DateTime.parse(a['sent_at'])
+            .isBefore(DateTime.parse(b['sent_at']))) {
+          return -1;
+        } else {
+          return 1;
+        }
+      } else if (a['sender_uid'] == user['data']['uid'] &&
+          b['sender_uid'] == user['data']['uid']) {
+        // User is sender of both chats
+        // Order chats chronologically
+        if (DateTime.parse(a['sent_at'])
+            .isBefore(DateTime.parse(b['sent_at']))) {
+          return -1;
+        } else {
+          return 1;
+        }
+      } else {
+        // User is both recipient and sender
+        // Check of which chat is recipient and give priority
+        if (b['recipient_uid'] == user['data']['uid']) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+    },
+  );
+
+  // Sort expired chats
+  _expiredChats.sort(
+    ((a, b) {
+      // Order chats chronologically
+      if (DateTime.parse(a['sent_at']).isBefore(DateTime.parse(b['sent_at']))) {
+        return -1;
+      } else {
+        return 1;
+      }
+    }),
+  );
+
+  // Add expired chats
+  List _chats = [..._expiredChats, ..._activeChats];
+
+  // Store chats locally
+  user['chats'] = _chats;
+
+  // Update count of total friends
+  // Check if user has friends
+  if (user['data']['friends'] != null) {
+    // User has friends
+    await firestore.collection('uids').doc(user['data']['uid']).update(
+      {
+        'total_friends': user['data']['friends'].length,
+      },
+    );
+  } else {
+    // User hasn't friends
+    await firestore.collection('uids').doc(user['data']['uid']).update(
+      {
+        'total_friends': 0,
+      },
+    );
   }
+
+  // Schedule local notifications for active chats
+  int index = 0;
+  for (Map _chat in _activeChats) {
+    index = index + 1;
+
+    // Check if user is recipient of chat
+    if (_chat['recipient_uid'] == user['data']['uid']) {
+      // User if recipient of the chat
+      // Check if a notification for this chat is already scheduled
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      int? notificationId = prefs.getInt(_chat['sender_uid']);
+
+      if (notificationId != null) {
+        // A notification is already scheduled
+        // Shut down function
+        return _chats;
+      }
+
+      // Fetch date for the notification
+      initializeTimeZones();
+      TZDateTime notificationDate =
+          TZDateTime.parse(local, _chat['sent_at']).add(
+        const Duration(
+          minutes: 1380,
+        ),
+      );
+
+      // Check if date of notification already occurred
+      if (DateTime.now().toLocal().isAfter(notificationDate)) {
+        // Date of notification already occurred
+        // Shut down function
+        return _chats;
+      }
+
+      // Generate a notification's UID
+      notificationId = DateTime.now().millisecondsSinceEpoch ~/ 1000 + index;
+
+      // Store notification's UID locally
+      prefs.setInt(_chat['sender_uid'], notificationId);
+
+      // Schedule local notification
+      LocalNotificationService.scheduleNotification(
+        _chat['sender_username'],
+        notificationId,
+        notificationDate,
+      );
+    }
+  }
+
+  return _chats;
 }
