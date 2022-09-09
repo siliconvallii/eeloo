1885e11e6bd3aa874b3a7449e8b84f08e30193aa
Filippo Valli <filippovalli2003@gmail.com>
2022-09-09 17:37:33 +0200

    Add ThemeData to MaterialData

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:eeloo/screens/initial_screen.dart';
 import 'package:eeloo/screens/sign_in_screen.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_app_badger/flutter_app_badger.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 // notification arrives when app is in background
@@ -83,6 +84,34 @@ class _EelooState extends State<Eeloo> {
             const AccountSettingsScreen(),
         '/info': (BuildContext context) => const InfoScreen(),
       },
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: const Color(0xff2E2E2E),
+          centerTitle: true,
+          elevation: 0,
+          foregroundColor: const Color(0xffFFFFFF),
+          titleTextStyle: GoogleFonts.alata(
+            fontSize: 24,
+          ),
+        ),
+        scaffoldBackgroundColor: const Color(0xff121212),
+        colorScheme: const ColorScheme(
+          brightness: Brightness.dark,
+          primary: Color(0xffBC91F8),
+          onPrimary: Color(0xffFFFFFF),
+          secondary: Color(0xff63D7C6),
+          onSecondary: Color(0xffFFFFFF),
+          error: Color(0xffFFFFFF),
+          onError: Color(0xffDC143C),
+          background: Color(0xff000000),
+          onBackground: Color(0xffFFFFFF),
+          surface: Color(0xff1E1E1E),
+          onSurface: Color(0xffFFFFFF),
+        ),
+        textTheme: TextTheme(
+          button: GoogleFonts.alata(),
+        ),
+      ),
     );
   }
 }
